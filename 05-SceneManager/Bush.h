9e138d1ad9c5592efee9e2d436da883c5bc551d7
NgCgThanh <thanhhsnvlxd2000@gmail.com>
2024-05-25 23:01:10 +0700

    add bush, tree and warppipe

---
+++ 05-SceneManager/Bush.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "GameObject.h"
+#include "Animation.h"
+#include "Animations.h"
+
+#define ID_ANI_BUSH 14000
+#define BUSH_WIDTH 16
+#define BUSH_BBOX_WIDTH 16
+#define BUSH_BBOX_HEIGHT 17
+
+class CBush : public CGameObject {
+public:
+	CBush(float x, float y) : CGameObject(x, y) {}
+	void Render();
+	void Update(DWORD dt) {}
+	void GetBoundingBox(float& l, float& t, float& r, float& b);
+};
